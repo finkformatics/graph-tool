9b4c5daa591c33089802a885e1081f0bc6da8128
Lukas Werner <lukas.werner@check24.de>
2020-04-15 08:38:04 +0200

    Started implementing graphs as classes

--- src/main/java/de/lwerner/graphTool/GraphTool.java
+++ src/main/java/de/lwerner/graphTool/GraphTool.java
@@ -2,27 +2,20 @@ package de.lwerner.graphTool;
 
 import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
-import javafx.scene.Cursor;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.StrokeLineCap;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
-import javafx.scene.text.TextBoundsType;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class GraphTool extends Application {
 
-    private final int DEFAULT_EDGE_WEIGHT = 1;
+    private static final int DEFAULT_EDGE_WEIGHT = 1;
 
-    double unitSize;
-    double orgSceneX, orgSceneY;
+    protected static double unitSize;
 
     public GraphTool() {
         Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
@@ -35,60 +28,21 @@ public class GraphTool extends Application {
         }
     }
 
-    private Vertex createVertex(double x, double y, double r) {
-        Circle circle = new Circle(x, y, r, Color.WHITE);
-        circle.setStroke(Color.BLACK);
-        circle.setStrokeWidth(2);
-
-        final Text vertexName = new Text(x, y, null);
-        vertexName.setFont(Font.font(25));
-        vertexName.setBoundsType(TextBoundsType.VISUAL);
-        vertexName.setTextAlignment(TextAlignment.CENTER);
-//        double w = vertexName.getBoundsInLocal().getWidth();
-//        double h = vertexName.getBoundsInLocal().getHeight();
-//
-//        vertexName.relocate(r - w / 2, r - h / 2);
-
-        vertexName.xProperty().bind(circle.centerXProperty());
-        vertexName.yProperty().bind(circle.centerYProperty());
-
-        circle.setCursor(Cursor.HAND);
-
-        circle.setOnMousePressed((t) -> {
-            orgSceneX = t.getSceneX();
-            orgSceneY = t.getSceneY();
-
-            Circle c = (Circle) (t.getSource());
-            c.toFront();
-            vertexName.toFront();
-        });
-
-        circle.setOnMouseDragged((t) -> {
-            double offsetX = t.getSceneX() - orgSceneX;
-            double offsetY = t.getSceneY() - orgSceneY;
-
-            Circle c = (Circle) (t.getSource());
-
-            c.setCenterX(c.getCenterX() + offsetX);
-            c.setCenterY(c.getCenterY() + offsetY);
-
-            orgSceneX = t.getSceneX();
-            orgSceneY = t.getSceneY();
-        });
-
-        return new Vertex(circle, vertexName);
+    private Vertex createVertex(int unitsX, int unitsY) {
+        return new Vertex(unitsX, unitsY);
     }
 
-    private Line connect(Circle c1, Circle c2) {
+    private Line connect(Vertex v1, Vertex v2) {
         Line line = new Line();
 
-        line.startXProperty().bind(c1.centerXProperty());
-        line.startYProperty().bind(c1.centerYProperty());
+        line.startXProperty().bind(v1.getComponents().layoutXProperty().add(1.5 * unitSize));
+        line.startYProperty().bind(v1.getComponents().layoutYProperty().add(1.5 * unitSize));
 
-        line.endXProperty().bind(c2.centerXProperty());
-        line.endYProperty().bind(c2.centerYProperty());
+        line.endXProperty().bind(v2.getComponents().layoutXProperty().add(1.5 * unitSize));
+        line.endYProperty().bind(v2.getComponents().layoutYProperty().add(1.5 * unitSize));
 
-        line.setStrokeWidth(2);
+        line.setStroke(Color.WHITE);
+        line.setStrokeWidth(1);
         line.setStrokeLineCap(StrokeLineCap.BUTT);
 
         return line;
@@ -97,45 +51,27 @@ public class GraphTool extends Application {
     @Override
     public void start(Stage stage) {
         Group root = new Group();
-        Scene scene = new Scene(root, 50 * unitSize, 30 * unitSize);
+        Scene scene = new Scene(root, 50 * unitSize, 30 * unitSize, Color.DARKSLATEBLUE);
 
         // circles
-        Vertex redCircle = createVertex(10 * unitSize, 5 * unitSize, 2 * unitSize);
-        Vertex blueCircle = createVertex(40 * unitSize, 10 * unitSize, 1.5 * unitSize);
-        Vertex greenCircle = createVertex(25 * unitSize, 20 * unitSize, 4 * unitSize);
+        Vertex redCircle = createVertex(10, 5);
+        Vertex blueCircle = createVertex(40, 10);
+        Vertex greenCircle = createVertex(25, 20);
 
-        Line line1 = connect(redCircle.getCircle(), blueCircle.getCircle());
-        Line line2 = connect(redCircle.getCircle(), greenCircle.getCircle());
-        Line line3 = connect(greenCircle.getCircle(), blueCircle.getCircle());
+        Line line1 = connect(redCircle, blueCircle);
+        Line line2 = connect(redCircle, greenCircle);
+        Line line3 = connect(greenCircle, blueCircle);
 
-//        StackPane redStack = new StackPane(redCircle.getCircle(), redCircle.getText());
-//        StackPane blueStack = new StackPane(blueCircle.getCircle(), blueCircle.getText());
-//        StackPane greenStack = new StackPane(greenCircle.getCircle(), greenCircle.getText());
-//
-//        root.getChildren().addAll(redStack, blueStack, greenStack);
-
-        // add the circles
-        root.getChildren().add(redCircle.getCircle());
-        root.getChildren().add(blueCircle.getCircle());
-        root.getChildren().add(greenCircle.getCircle());
-
-        root.getChildren().add(redCircle.getText());
-        root.getChildren().add(blueCircle.getText());
-        root.getChildren().add(greenCircle.getText());
+        root.getChildren().addAll(redCircle.getComponents(), blueCircle.getComponents(), greenCircle.getComponents());
 
         // add the lines
         root.getChildren().add(line1);
         root.getChildren().add(line2);
         root.getChildren().add(line3);
 
-        // bring the circles to the front of the lines
-        redCircle.getCircle().toFront();
-        blueCircle.getCircle().toFront();
-        greenCircle.getCircle().toFront();
-
-        redCircle.getText().toFront();
-        blueCircle.getText().toFront();
-        greenCircle.getText().toFront();
+        redCircle.getComponents().toFront();
+        blueCircle.getComponents().toFront();
+        greenCircle.getComponents().toFront();
 
         stage.setScene(scene);
         stage.show();
--- src/main/java/de/lwerner/graphTool/Vertex.java
+++ src/main/java/de/lwerner/graphTool/Vertex.java
@@ -1,19 +1,89 @@
 package de.lwerner.graphTool;
 
+import javafx.scene.Cursor;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+
+import static de.lwerner.graphTool.GraphTool.unitSize;
 
 public class Vertex {
 
     private static int vertexCount;
 
+    private final StackPane components;
     private final Circle circle;
     private final Text text;
 
-    public Vertex(Circle circle, Text text) {
-        this.circle = circle;
-        this.text = text;
-        this.text.setText("" + ++vertexCount);
+    private int unitsX;
+    private int unitsY;
+
+    private double orgSceneX;
+    private double orgSceneY;
+
+    public Vertex(int unitsX, int unitsY) {
+        this.unitsX = unitsX;
+        this.unitsY = unitsY;
+
+        this.circle = new Circle(1.5 * unitSize, Color.DARKSLATEBLUE);
+        this.text = new Text("" + ++vertexCount);
+        this.components = new StackPane();
+
+        buildComponents();
+    }
+
+    private void buildComponents() {
+        components.setLayoutX(unitsX * unitSize);
+        components.setLayoutY(unitsY * unitSize);
+
+        circle.setStroke(Color.WHITE);
+        circle.setStrokeWidth(1);
+
+        text.setFont(Font.font(unitSize));
+        text.setFill(Color.WHITE);
+
+        circle.setCursor(Cursor.HAND);
+        text.setCursor(Cursor.HAND);
+
+        circle.setOnMousePressed((t) -> {
+            orgSceneX = t.getSceneX();
+            orgSceneY = t.getSceneY();
+        });
+
+        text.setOnMousePressed((t) -> {
+            orgSceneX = t.getSceneX();
+            orgSceneY = t.getSceneY();
+
+            components.toFront();
+        });
+
+        circle.setOnMouseDragged((t) -> {
+            double offsetX = t.getSceneX() - orgSceneX;
+            double offsetY = t.getSceneY() - orgSceneY;
+
+            components.relocate(components.getLayoutX() + offsetX, components.getLayoutY() + offsetY);
+
+            orgSceneX = t.getSceneX();
+            orgSceneY = t.getSceneY();
+
+            components.toFront();
+        });
+
+        text.setOnMouseDragged((t) -> {
+            double offsetX = t.getSceneX() - orgSceneX;
+            double offsetY = t.getSceneY() - orgSceneY;
+
+            components.relocate(components.getLayoutX() + offsetX, components.getLayoutY() + offsetY);
+
+            orgSceneX = t.getSceneX();
+            orgSceneY = t.getSceneY();
+
+            components.toFront();
+        });
+
+        components.getChildren().addAll(circle, text);
     }
 
     public Circle getCircle() {
@@ -22,6 +92,10 @@ public class Vertex {
 
     public Text getText() {
         return text;
+    }
+
+    public StackPane getComponents() {
+        return components;
     }
 
 }
